372639a030d74fcf2b97832fb17cc53f4cd8481b
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-05-29 17:11:58 +0100

    Add SeaTalk stuff

--- router/src/main/java/org/vesalainen/nmea/router/SeaTalkChannel.java
+++ router/src/main/java/org/vesalainen/nmea/router/SeaTalkChannel.java
@@ -16,13 +16,71 @@
  */
 package org.vesalainen.nmea.router;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ScatteringByteChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.spi.AbstractSelectableChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.List;
+import java.util.Set;
 import org.vesalainen.comm.channel.SerialChannel;
+import org.vesalainen.comm.channel.SerialSelectorProvider;
+import org.vesalainen.nio.RingByteBuffer;
 
 /**
  *
  * @author tkv
  */
-public class SeaTalkChannel 
+public class SeaTalkChannel extends AbstractSelectableChannel implements ScatteringByteChannel
 {
+    private final SerialChannel channel;
+    private final RingByteBuffer ring = new RingByteBuffer(100, true);
+
+    public SeaTalkChannel(SerialChannel channel)
+    {
+        super(SerialSelectorProvider.provider());
+        this.channel = channel;
+    }
+
+    private final ByteBuffer[] readArray = new ByteBuffer[1];
+    @Override
+    public int read(ByteBuffer dst) throws IOException
+    {
+        readArray[0] = dst;
+        return (int) read(readArray);
+    }
+
+    @Override
+    public long read(ByteBuffer[] dsts, int offset, int length) throws IOException
+    {
+        return channel.read(dsts, offset, length);
+    }
+
+    @Override
+    public long read(ByteBuffer[] dsts) throws IOException
+    {
+        return read(dsts, 0, dsts.length);
+    }
+
+    @Override
+    public int validOps()
+    {
+        return channel.validOps();
+    }
+
+    @Override
+    protected void implCloseSelectableChannel() throws IOException
+    {
+        channel.close();
+    }
+
+    @Override
+    protected void implConfigureBlocking(boolean block) throws IOException
+    {
+        channel.configureBlocking(block);
+    }
     
 }
