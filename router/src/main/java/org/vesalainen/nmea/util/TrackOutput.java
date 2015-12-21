98c7301ffb08601d478ee72afc300a694abb5072
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-12-21 05:03:51 -0200

    dev

--- router/src/main/java/org/vesalainen/nmea/util/TrackOutput.java
+++ router/src/main/java/org/vesalainen/nmea/util/TrackOutput.java
@@ -25,7 +25,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 import org.vesalainen.io.CompressedOutput;
-import org.vesalainen.util.logging.JavaLogging;
 
 /**
  *
