6c55d368b2b8a854aa8cf23e3092d53b3e3c9ba7
Timo Vesalainen <timo.vesalainen@iki.fi>
2016-09-28 17:30:13 -0400

    dev

---
+++ track-plotter/src/main/java/org/vesalainen/nmea/trackplotter/Plotter.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vesalainen.nmea.trackplotter;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import org.vesalainen.nmea.util.TrackPlotter;
+import org.vesalainen.util.CmdArgs;
+
+/**
+ *
+ * @author tkv
+ */
+public class Plotter extends CmdArgs
+{
+
+    public Plotter()
+    {
+        addArgument(File.class, "file");
+        addOption("-w", "width", null, 1000);
+        addOption("-h", "height", null, 1000);
+        addOption("-ext", "[svg|jpg|png|gif]", null, "png");
+    }
+    
+    public void plot() throws IOException
+    {
+        File file = (File) getArgument("file");
+        String ext = (String) getOption("-ext");
+        int width = (int) getOption("-w");
+        int height = (int) getOption("-h");
+        TrackPlotter trackPlotter = new TrackPlotter(file, width, height, Color.BLACK, Color.WHITE);
+        String path = file.getPath();
+        int idx = path.lastIndexOf('.');
+        if (idx != -1)
+        {
+            path = path.substring(0, idx);
+        }
+        path = path+'.'+ext;
+        trackPlotter.plot(path, ext);
+    }
+    
+    public static void main(String... args)
+    {
+        try
+        {
+            Plotter plotter = new Plotter();
+            plotter.setArgs(args);
+            plotter.plot();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ track-plotter/src/test/java/org/vesalainen/nmea/trackplotter/PlotterTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.nmea.trackplotter;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tkv
+ */
+public class PlotterTest
+{
+    
+    public PlotterTest()
+    {
+    }
+
+    @Test
+    public void test1()
+    {
+        try
+        {
+            URL url = PlotterTest.class.getResource("/20160612200334.trc");
+            File file = new File(url.toURI());
+            Plotter.main(file.getPath());
+        }
+        catch (URISyntaxException ex)
+        {
+            fail(ex.getMessage());
+            Logger.getLogger(PlotterTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
